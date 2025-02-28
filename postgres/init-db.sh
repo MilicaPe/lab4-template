3dad11515200f29a5d60692cd86ec0e61a1286ac
MilicaPe <mpetrovic764307@gmail.com>
2025-02-28 19:42:39 +0300

    added helm charts

---
+++ postgres/init-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Логируем запуск
+echo "Running initialization scripts..."
+
+# Выполняем SQL-скрипты в нужном порядке
+psql -U postgres -f /docker-entrypoint-initdb.d/10-create-user.sql
+psql -U postgres -f /docker-entrypoint-initdb.d/20-create-databases.sh
+psql -U postgres -f /docker-entrypoint-initdb.d/30-create-table.sql
+psql -U postgres -f /docker-entrypoint-initdb.d/40-insert-data.sql
+
+echo "Database initialization complete."
